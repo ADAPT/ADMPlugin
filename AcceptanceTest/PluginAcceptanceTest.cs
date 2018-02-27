50877ceb48cd303d3ceae60c5feb6deecd4af823
Tarak Reddy <reddylingat@johndeere.com>
2018-02-27 13:56:39 -0600

    Make ADM plugin compatible with version 1.1.0.8 datacards.
    Signed-off-by: Tarak Reddy <reddylingat@johndeere.com>

---
+++ AcceptanceTest/PluginAcceptanceTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ADMPlugin;
+using NUnit.Framework;
+using TestUtilities;
+
+namespace AcceptanceTest
+{
+    [TestFixture]
+    public class PluginAcceptanceTest
+    {
+        private string _cardPath;
+        
+        [Test]
+        public void GivenOlderVersionSetupCardWhenImportedToVersionWithChangedPropertyAndTypeNamesThenImportsToTheNewerModel()
+        {
+            _cardPath = DatacardUtility.WriteDataCard("adm_setup_v1_0_8");
+
+            var plugin = new Plugin();
+            var applicationDataModel = plugin.Import(_cardPath);
+
+            Assert.IsNotNull(applicationDataModel);
+        }
+
+        [Test]
+        public void GivenOlderVersionDocCardWhenImportedToVersionWithChangedPropertyAndTypeNamesThenImportsToTheNewerModel()
+        {
+            _cardPath = DatacardUtility.WriteDataCard("adm_doc_v1_0_8");
+
+            var plugin = new Plugin();
+            var applicationDataModel = plugin.Import(_cardPath);
+
+            var id = applicationDataModel.First().Catalog.Connectors.First().DeviceElementConfigurationId;
+
+            Assert.AreNotEqual(0, id);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            try
+            {
+                Directory.Delete(Path.GetDirectoryName(_cardPath), true);
+            }
+            catch { }
+        }
+    }
+}
--- TestUtilities/Datacards.Designer.cs
+++ TestUtilities/Datacards.Designer.cs
@@ -19,7 +19,7 @@ namespace TestUtilities {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Datacards {
@@ -57,6 +57,26 @@ namespace TestUtilities {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] adm_doc_v1_0_8 {
+            get {
+                object obj = ResourceManager.GetObject("adm_doc_v1_0_8", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] adm_setup_v1_0_8 {
+            get {
+                object obj = ResourceManager.GetObject("adm_setup_v1_0_8", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
